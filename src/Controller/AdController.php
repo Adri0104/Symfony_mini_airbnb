e69af076dfd5dd14085ca314e53acba76c3950e9
Adrien Colonna <adriencolonna@macbook-pro-de-adrien.home>
2019-10-17 19:08:20 +0200

    Add show page

---
+++ src/Controller/AdController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads.index")
+     */
+    public function index(AdRepository $repo) : Response
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * @Route("/ads/{slug}", name="ads.show")
+     */
+    public function show(Ad $ad) : Response
+    {
+        return $this->render('ad/show.html.twig', compact('ad'));
+    }
+}
