f68feb9effdc5feff2133d54d22059f23448dc34
Adrien Colonna <adriencolonna@macbook-pro-de-adrien.home>
2019-10-22 19:38:00 +0200

    Add author

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,6 +80,11 @@ class User implements UserInterface
      */
     private $ads;
 
+    public function getFullName()
+    {
+        return "{$this->firstName} {$this->lastName}";
+    }
+
     /**
      * @ORM\PrePersist()
      * @ORM\PreUpdate()
